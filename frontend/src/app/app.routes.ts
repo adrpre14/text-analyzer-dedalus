02388df6c6fce4f16e854b3e19c82905f2c86abb
a.precaj <a.precaj@albansi.com>
2025-06-11 16:55:44 +0200

    first commit, initialization of frontend project

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./text-analyzer/text-analyzer').then(m => m.TextAnalyzer)
+  }
+];
